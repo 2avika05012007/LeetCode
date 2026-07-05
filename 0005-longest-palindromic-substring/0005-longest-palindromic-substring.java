759afe2273491627c485a533279c3fba3f86dfd3
Avika Narayan <2avika05012007@gmail.com>
2026-07-05 21:13:41 +0530

    Time: 14 ms (91%), Space: 43.5 MB (79.73%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -6,8 +6,7 @@ class Solution {
 
         int start = 0;
         int end = 0;
-
-        for (int i = 0; i < s.length(); i++) {
+    for (int i = 0; i < s.length(); i++) {
             int len1 = expand(s, i, i);       
             int len2 = expand(s, i, i + 1);   
             int len = Math.max(len1, len2);
@@ -20,7 +19,6 @@ class Solution {
 
         return s.substring(start, end + 1);
     }
-
     private int expand(String s, int left, int right) {
         while (left >= 0 && right < s.length()
                 && s.charAt(left) == s.charAt(right)) {
