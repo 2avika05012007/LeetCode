56756a08e4337a2aa6c33c4662a77b97a4b4aedc
Avika Narayan <2avika05012007@gmail.com>
2026-07-05 21:13:23 +0530

    Time: 14 ms (91%), Space: 43.1 MB (97.63%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -8,8 +8,8 @@ class Solution {
         int end = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expand(s, i, i);       // Odd-length palindrome
-            int len2 = expand(s, i, i + 1);   // Even-length palindrome
+            int len1 = expand(s, i, i);       
+            int len2 = expand(s, i, i + 1);   
             int len = Math.max(len1, len2);
 
             if (len > end - start + 1) {
