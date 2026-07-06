b4d2b81557e19d44c9b4579866be46cbc22a95d2
Avika Narayan <2avika05012007@gmail.com>
2026-07-06 20:07:16 +0530

    Time: 0 ms (100%), Space: 48 MB (57.5%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,24 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-
-        long power = n;
         double ans = 1.0;
 
-        if (power < 0) {
-            x = 1 / x;
-            power = -power;
+        long pow = n;
+
+        if(n<0){
+            x = 1/x;
+            pow = -pow;
         }
+        while(pow>0){
+            if(pow%2 == 1){
+               ans *= x;
 
-        while (power > 0) {
-
-            if (power % 2 == 1) {
-                ans *= x;
             }
-
-            x *= x;
-            power /= 2;
+            x*=x;
+            pow/=2;
         }
-
         return ans;
     }
 }
