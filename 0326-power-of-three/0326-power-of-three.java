54520720a8efd6d601f7a94d4ffac36d36eddeb0
Avika Narayan <2avika05012007@gmail.com>
2026-07-07 22:49:41 +0530

    Time: 8 ms (91.61%), Space: 45.8 MB (83.59%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -4,13 +4,9 @@ class Solution {
          if (n <= 0) {
             return false;
         }
-        while(i<20){
-            if(Math.pow(3, i) == n){
-                return true;
-            }
-            i++;
-
+        while(n%3 == 0){
+            n/=3;
         }
-        return false;
+        return n == 1;
     }
 }
