f4eb4c879b938ef1c03b439da032f7015c5379f1
Avika Narayan <2avika05012007@gmail.com>
2026-07-08 12:47:39 +0530

    Time: 2819 ms (2.98%), Space: 42.6 MB (49.85%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -3,7 +3,22 @@ class Solution {
         if(dividend == -2147483648&&divisor == -1){
             return 2147483647;
         }
-        
-       return dividend/divisor;
+        int count = 0;
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+        if (divisor == -1){
+            return -dividend;
+        }
+        while(a>=b){
+            count ++;
+            a-=b;
+        }
+        if(dividend<0&&divisor<0){
+            return count;
+        }
+        if(dividend<0||divisor<0){
+            return -count;
+        }
+        return count;
     }
 }
