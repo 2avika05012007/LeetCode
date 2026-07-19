871addc24158998a4054acdeea4c4f7490f65dae
Avika Narayan <2avika05012007@gmail.com>
2026-07-19 19:55:14 +0530

    Time: 4 ms (41.83%), Space: 47.8 MB (89.47%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,19 +1,18 @@
 import java.util.*;
 class Solution {
     public int[] singleNumber(int[] nums) {
-        Stack<Integer> st = new Stack<>();
-        for(int i: nums){
-            if(st.contains(i)){
-                st.remove(Integer.valueOf(i));
-            }
-            else{st.push(i);}
+        HashSet<Integer> set = new HashSet<>();
+        for(int number : nums){
+            if(set.contains(number))
+                set.remove(number);
+            else
+                set.add(number);
         }
-        int[] ans = new int[st.size()];
-        int size = st.size();
-
-for(int i = 0; i < size; i++){
-    ans[i] = st.pop();
-}
+        int[] ans = new int[2];
+        int i = 0;
+        for(int number : set){
+            ans[i++] = number;
+        }
         return ans;
     }
 }
