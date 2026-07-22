cdd63153cfc28b999369f7e38efdeece54da5483
Avika Narayan <2avika05012007@gmail.com>
2026-07-22 13:10:55 +0530

    Time: 4 ms (22.39%), Space: 202.4 MB (76.25%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -13,20 +13,26 @@ class Solution {
         if (head == null || head.next == null) {
             return null;
         }
+        
         int size = 0;
         ListNode temp = head;
+
         while (temp != null) {
             size++;
             temp = temp.next;
         }
         int mid = size / 2;
         temp = head;
+
         ListNode pre = null;
+
         for (int i = 0; i < mid; i++) {
             pre = temp;
             temp = temp.next;
         }
+
         pre.next = temp.next;
+
         return head;
     }
 }
