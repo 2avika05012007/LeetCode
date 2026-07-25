53ffd380bb3d212efc1c16e807ae4c49cd96cee5
Avika Narayan <2avika05012007@gmail.com>
2026-07-25 15:50:53 +0530

    Time: 1962 ms (5.31%), Space: 42 MB (62.5%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int ans = 0;
+        while(target!=startValue){
+            if(target%2==0 && target>startValue){
+                target/=2;
+                ans++;
+            }
+            else{
+                target++;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
