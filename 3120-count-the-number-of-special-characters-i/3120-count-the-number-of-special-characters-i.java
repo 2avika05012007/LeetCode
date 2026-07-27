2a600178527f8c98bc0830c48833c15617be19a3
Avika Narayan <2avika05012007@gmail.com>
2026-07-27 21:30:52 +0530

    Time: 1 ms (100%), Space: 43.5 MB (89.22%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,29 +1,18 @@
-import java.util.*;
 class Solution {
     public int numberOfSpecialChars(String word) {
-        Stack<Character> st = new Stack<>();
-        if (word.length() == 0) return 0;
-        st.push(word.charAt(0));
-        HashSet<Character> set = new HashSet<>();
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
         for (char ch : word.toCharArray()) {
-            if (Character.isLowerCase(ch)) {
-                if (st.contains(Character.toUpperCase(ch))) {
-                    st.remove(Character.valueOf(Character.toUpperCase(ch)));
-                    set.add(ch);
-
-                } else {
-                    st.push(ch);
-                }
-            }
-            if (Character.isUpperCase(ch)) {
-                if (st.contains(Character.toLowerCase(ch))) {
-                    st.remove(Character.valueOf(Character.toLowerCase(ch)));
-                    set.add(Character.toLowerCase(ch));
-                } else {
-                    st.push(ch);
-                }
-            }
+            if (Character.isLowerCase(ch))
+                lower[ch - 'a'] = true;
+            else
+                upper[ch - 'A'] = true;
         }
-        return set.size();
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] && upper[i])
+                count++;
+        }
+        return count;
     }
 }
