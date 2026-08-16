93ad424c6671fc0cc03cde14e4e772970dac3e88
Avika Narayan <2avika05012007@gmail.com>
2026-08-16 17:06:23 +0530

    Time: 2 ms (8.09%), Space: 43.8 MB (77.12%) - LeetHub

--- 4006-count-valid-prefixes/4006-count-valid-prefixes.java
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.java
@@ -1,28 +1,18 @@
 class Solution {
-
-    public boolean bool(StringBuilder s) {
+    public int countValidPrefixes(String s) {
         int zero = 0;
         int nonZero = 0;
+        int ans = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '0') {
+
+            if(s.charAt(i) == '0') {
                 zero++;
-            } else {
+            }
+            else {
                 nonZero++;
             }
-        }
-
-        return Math.abs(nonZero - zero) <= 1;
-    }
-
-    public int countValidPrefixes(String s) {
-        int ans = 0;
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            sb.append(s.charAt(i));
-
-            if (bool(sb)) {
+            if (Math.abs(zero - nonZero) <= 1) {
                 ans++;
             }
         }
