54205f963e6ef203008c4c545433556d765aac88
Avika Narayan <2avika05012007@gmail.com>
2026-08-27 08:39:17 +0530

    Time: 2 ms (91.94%), Space: 45 MB (61.29%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
@@ -4,39 +4,30 @@ class Solution {
 
         int[] count = new int[26];
 
-        // Count characters in s
         for (char c : s.toCharArray()) {
             count[c - 'a']++;
         }
 
-        // Try to match target from left to right
         int i = 0;
 
         while (i < n && count[target.charAt(i) - 'a'] > 0) {
             count[target.charAt(i) - 'a']--;
             i++;
         }
-
-        // Backtrack
         for (int pos = i; pos >= 0; pos--) {
 
             if (pos < n) {
-
-                // Try every character greater than target[pos]
                 for (int c = target.charAt(pos) - 'a' + 1; c < 26; c++) {
 
                     if (count[c] > 0) {
 
                         StringBuilder ans = new StringBuilder();
 
-                        // Prefix remains same
                         ans.append(target.substring(0, pos));
 
-                        // Choose the smallest larger character
                         ans.append((char) ('a' + c));
                         count[c]--;
 
-                        // Remaining characters in sorted order
                         for (int x = 0; x < 26; x++) {
                             while (count[x] > 0) {
                                 ans.append((char) ('a' + x));
@@ -48,8 +39,6 @@ class Solution {
                     }
                 }
             }
-
-            // Restore character while backtracking
             if (pos > 0) {
                 count[target.charAt(pos - 1) - 'a']++;
             }
