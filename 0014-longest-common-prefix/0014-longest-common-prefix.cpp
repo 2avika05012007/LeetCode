6ea1a841b889d536fe96520640fab50b5cf850c0
Avika Narayan <2avika05012007@gmail.com>
2026-08-31 19:42:54 +0530

    Time: 0 ms (100%), Space: 11.8 MB (80.12%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,9 +6,7 @@ public:
         for (int i = 1; i < strs.size(); i++) {
             int j = 0;
 
-            while (j < prefix.size() &&
-                   j < strs[i].size() &&
-                   prefix[j] == strs[i][j]) {
+            while (j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j]) {
                 j++;
             }
 
